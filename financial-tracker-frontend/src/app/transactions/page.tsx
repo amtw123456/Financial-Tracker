75e8e1e4447bf68658110c38ca37b065ddffcdc3
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2024-07-28 14:16:49 +0800

    feat: added pages for transactions and dashboard

--- financial-tracker-frontend/src/app/transactions/page.tsx
+++ financial-tracker-frontend/src/app/transactions/page.tsx
@@ -1,8 +1,8 @@
 
-export default function Dashboard() {
+export default function Transactions() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            dashboard
+            transactions
         </main>
     );
 }
