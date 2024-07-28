aaeb66ab1c5d70779a35df022cb62d72f11f5a9c
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2024-07-29 01:30:15 +0800

    feat: addded navbar

--- financial-tracker-frontend/src/app/page.tsx
+++ financial-tracker-frontend/src/app/page.tsx
@@ -1,7 +1,33 @@
+import React from "react";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Poknat
-    </main>
+    <>
+      <div className="w-full h-20 bg-emerald-800 sticky top-0">
+        <div className="container mx-auto px-4 h-full">
+          <div className="flex justify-between items-center h-full">
+            <ul className="hidden md:flex gap-x-6 text-white">
+              <li>
+                <Link href="/about">
+                  <p>About Us</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/services">
+                  <p>Services</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/contacts">
+                  <p>Contacts</p>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
   );
-}
+};
+
