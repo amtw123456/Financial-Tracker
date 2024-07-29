19ca12b426ccb44b6dd1075ac6e48ad10d0a1490
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2024-07-29 09:49:37 +0800

    feat: added routing

--- financial-tracker-frontend/src/app/page.tsx
+++ financial-tracker-frontend/src/app/page.tsx
@@ -3,31 +3,31 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="w-full h-20 bg-emerald-800 sticky top-0">
         <div className="container mx-auto px-4 h-full">
           <div className="flex justify-between items-center h-full">
             <ul className="hidden md:flex gap-x-6 text-white">
               <li>
-                <Link href="/about">
-                  <p>About Us</p>
+                <Link href="/dashboard">
+                  <p>Dashboard</p>
                 </Link>
               </li>
               <li>
-                <Link href="/services">
-                  <p>Services</p>
+                <Link href="/login">
+                  <p>Login</p>
                 </Link>
               </li>
               <li>
-                <Link href="/contacts">
-                  <p>Contacts</p>
+                <Link href="/signin">
+                  <p>Signin</p>
                 </Link>
               </li>
             </ul>
           </div>
         </div>
       </div>
-    </>
+    </main>
   );
 };
 
