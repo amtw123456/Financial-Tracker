94bff157d7d327cb17d42ca57de8313b6cc9ac1c
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2024-08-11 15:41:13 +0800

    feat: added profile picture image and dropdown items

--- financial-tracker-frontend/src/app/dashboard/page.tsx
+++ financial-tracker-frontend/src/app/dashboard/page.tsx
@@ -22,7 +22,7 @@ export default function Dashboard() {
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                     d="M6 18L18 6M6 6l12 12" />
                             </svg>
-                            Messages
+                            Transactions
                         </a>
                         <a href="#" className="flex items-center px-4 py-2 mt-2 text-gray-100 hover:bg-gray-700">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24"
@@ -30,7 +30,7 @@ export default function Dashboard() {
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                     d="M13 10V3L4 14h7v7l9-11h-7z" />
                             </svg>
-                            Settings
+                            About
                         </a>
                     </nav>
                 </div>
@@ -49,14 +49,31 @@ export default function Dashboard() {
                         </button>
                         <input className="mx-4 w-full border rounded-md px-4 py-2" type="text" placeholder="Search" />
                     </div>
-                    <div className="flex items-center pr-4">
-
-                        <button
-                            className="flex items-center text-gray-500 hover:text-gray-700 focus:outline-none focus:text-gray-700">
-                            logout
-                        </button>
+                    <div className="relative inline-block text-left">
+                        <div className="group relative cursor-pointer py-2">
+                            <div className="flex items-center justify-between space-x-5 bg-white px-4">
+                                <div>
+                                    <button type="button" className="flex rounded-full text-sm text-white focus:bg-sky-900 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-sky-900" id="user-menu-button">
+                                        <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=320&amp;h=320&amp;q=80" alt="" />
+                                    </button>
+                                </div>
+                            </div>
+                            <div className="absolute z-50 right-0 mt-2 hidden w-48 max-w-screen-sm bg-gray-100 py-1 px-4 text-gray-800 shadow-xl group-hover:block">
+                                <a className="my-2 block border-b border-gray-100 py-1 font-semibold text-gray-500 hover:text-black">
+                                    Sunday
+                                </a>
+                                <a className="my-2 block border-b border-gray-100 py-1 font-semibold text-gray-500 hover:text-black">
+                                    Monday
+                                </a>
+                                <a className="my-2 block border-b border-gray-100 py-1 font-semibold text-gray-500 hover:text-black">
+                                    Tuesday
+                                </a>
+                            </div>
+                        </div>
                     </div>
+
                 </div>
+
                 <div className="p-4">
                     {/* <h1 className="text-2xl font-bold">Welcome to my dashboard!</h1>
                     <p className="mt-2 text-gray-600">This is an example dashboard using Tailwind CSS.</p> */}
@@ -143,6 +160,6 @@ export default function Dashboard() {
                 </div>
             </div>
 
-        </div>
+        </div >
     );
 }
