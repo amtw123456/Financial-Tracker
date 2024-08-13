cb9908fea5a3b318905d0b18c49689839532b58f
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2024-08-13 13:00:45 +0800

    feat: added nextUI library for built in components

--- financial-tracker-frontend/src/app/page.tsx
+++ financial-tracker-frontend/src/app/page.tsx
@@ -1,33 +1,42 @@
 import React from "react";
 import Link from "next/link";
+import { NextUIProvider } from "@nextui-org/react";
 
-export default function Home() {
+export default function App() {
   return (
-    <main  >
-      <div className="w-full h-20 bg-emerald-800 sticky top-0">
-        <div className="container mx-auto px-4 h-full">
-          <div className="flex justify-between items-center h-full">
-            <ul className="hidden md:flex gap-x-6 text-white">
-              <li>
-                <Link href="/dashboard">
-                  <p>Dashboard</p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/login">
-                  <p>Login</p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/signup">
-                  <p>Signin</p>
-                </Link>
-              </li>
-            </ul>
+    <NextUIProvider>
+      <main  >
+        <div className="w-full h-20 bg-emerald-800 sticky top-0">
+          <div className="container mx-auto px-4 h-full">
+            <div className="flex justify-between items-center h-full">
+              <ul className="hidden md:flex gap-x-6 text-white">
+                <li>
+                  <Link href="/dashboard">
+                    <p>Dashboard</p>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/login">
+                    <p>Login</p>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/signup">
+                    <p>Signin</p>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/transactions">
+                    <p>transaction</p>
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </NextUIProvider>
+
   );
 };
 
--- financial-tracker-frontend/src/app/transactions/page.tsx
+++ financial-tracker-frontend/src/app/transactions/page.tsx
@@ -1,8 +1,13 @@
+import { DateRangePicker } from "@nextui-org/react";
 
 export default function Transactions() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            transactions
+            {/* transactions */}
+            <DateRangePicker
+                label="Stay duration"
+                className="max-w-xs"
+            />
         </main>
     );
 }
