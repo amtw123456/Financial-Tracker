97c482dc0f888db022a3e92e615d41d7a02d9ae9
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2024-09-14 00:19:49 +0800

    fix: fix middleware routing issue

--- financial-tracker-frontend/src/middleware.ts
+++ financial-tracker-frontend/src/middleware.ts
@@ -3,37 +3,45 @@ import { NextRequest, NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
 
 export async function middleware(request: NextRequest) {
-    // Retrieve the cookie store
+    const url = request.nextUrl.clone();
     const cookieStore = cookies();
     const token = cookieStore.get('jwtToken'); // Get the JWT token from cookies
 
-    // If the token does not exist, allow access to the requested routes
-    if (!token) {
-        return NextResponse.next();
-    }
+    // If the token exists, verify its validity
+    if (token) {
+        try {
+            // Call the /isAuth endpoint to verify the token
+            const response = await fetch('http://localhost:3000/api/isauth', {
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearer ${token.value}`, // Pass the JWT token as a Bearer token
+                },
+            });
 
-    try {
-        // Call the /isAuth endpoint to verify the token
-        const response = await fetch('http://localhost:3000/api/isauth', {
-            method: 'GET',
-            headers: {
-                Authorization: `Bearer ${token.value}`, // Pass the JWT token as a Bearer token
-            },
-        });
+            // If the token is valid, redirect to the dashboard if accessing login or register
+            if (response.ok) {
+                if (url.pathname === '/login' || url.pathname === '/signup') {
 
-        // If the token is valid, redirect to the dashboard
-        if (response.ok) {
-            return NextResponse.redirect(new URL('/dashboard', request.url));
+                    return NextResponse.redirect(new URL('/dashboard', request.url));
+                }
+                // Proceed with the request if authenticated and accessing other routes
+                return NextResponse.next();
+            }
+        } catch (error) {
+            console.error('Error checking authentication:', error);
+            return NextResponse.redirect(new URL('/login', request.url));
         }
-    } catch (error) {
-        console.error('Error checking authentication:', error);
     }
 
-    // Proceed with the request if the token is invalid or the call fails
+    if (url.pathname !== '/login' && url.pathname !== '/signup') {
+        return NextResponse.redirect(new URL('/login', request.url));
+    }
+
+    // Allow access to /login and /register pages for unauthenticated users
     return NextResponse.next();
 }
 
 // Apply middleware only to these routes
 export const config = {
-    matcher: ['/register', '/login'],
+    matcher: ['/transactions', '/dashboard', '/analytics', '/login', '/signup', '/'],
 };
