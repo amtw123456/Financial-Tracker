3eb9f69c80195080f4b32523ae497d0be1ce4deb
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2024-09-14 18:38:29 +0800

    chore: refactor server sided api call to authentication folder for easier tracking

---
+++ financial-tracker-frontend/src/app/api/authentication/login/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from 'next/server';
+import axios from 'axios';
+import { cookies } from 'next/headers';
+
+export async function POST(req: NextRequest) {
+    const { username, password } = await req.json(); // Extract credentials from the request body
+
+    try {
+        // Replace the URL with your actual backend login endpoint
+        const response = await axios.post('http://localhost:8080/login', {
+            "username": username,
+            "password": password
+        }, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const token = response.data;
+        // Set cookies for authentication
+        cookies().set('jwtToken', token, { maxAge: 60 * 60 });
+        cookies().set('email', username, { maxAge: 60 * 60 });
+
+        return NextResponse.json({ message: 'Login successful' }, { status: 200 });
+
+    } catch (error) {
+        // console.error('Login failed:', error);
+        return NextResponse.json({ message: 'Login failed' }, { status: 401 });
+    }
+}
