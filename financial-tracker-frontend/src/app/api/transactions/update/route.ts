9b041b3da1a167263adc7660745c6447618b382c
Ronel Dylan Joshua A. Dolor <80044625+amtw123456@users.noreply.github.com>
2024-09-22 17:51:55 +0800

    chore: added code inside empty routes to prevent vercel error

---
+++ financial-tracker-frontend/src/app/api/transactions/update/route.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from 'next/server';
+import axios from 'axios';
+import { cookies } from 'next/headers';
+
+export async function GET() {
+
+    try {
+        // Retrieve token and userId from cookies
+        const token = cookies().get('jwtToken')?.value;
+        const userId = cookies().get('userId')?.value;
+
+        if (!token || !userId) {
+            return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+        }
+
+        const response = await axios.get(
+            `http://localhost:8080/transaction/getAll/${userId}`,
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        );
+
+        // Return success response
+        return NextResponse.json({ message: 'Get all user specific Transactions Successfully', listOfUserTransactions: response.data }, { status: 200 });
+
+    } catch (error) {
+
+        return NextResponse.json({ message: 'Get all user specific Transactions Failed' }, { status: 500 });
+    }
+}
